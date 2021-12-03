31db15d9d99bfe3b434d73346a934e66cb6d300f
Jens Axel Søgaard <jensaxel@soegaard.net>
2021-12-03 18:41:22 +0100

    Preserve source locations in reader and module-begin

--- lang/reader.rkt
+++ lang/reader.rkt
@@ -7,5 +7,5 @@
 
 (define (read-syntax-p5 src in)
   (define module-datum `(module p5-syntax p5/p5
-                          ,(port->list read in)))
+                          ,(port->list (λ (in) (read-syntax src in)) in)))
   (datum->syntax #f module-datum))
